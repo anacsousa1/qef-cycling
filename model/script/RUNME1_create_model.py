52aba8294cdc56331a921176fc62248cb18bd21c
Ana de Sousa <anacsousa1@gmail.com>
2020-05-29 17:17:26 -0300

    Better organization of file

--- model/script/RUNME1_create_model.py
+++ model/script/RUNME1_create_model.py
@@ -1,100 +1,78 @@
-# Import OpenSim libraries
+# IMPORT OPENSIM LIBRARIES
 import opensim as osim
 
-# Import other libraries
+# IMPORT OTHER LIBRARIES
 import pathlib  # get current path
 import os.path  # create path
 import numpy    # math
 
 # DEFINE KEY MODEL VARIABLES 
-pelvisWidth = 0.20;     # ex
-thighLength = 0.40;     # ex
-shankLength = 0.435;    # ex
-
-# Paths and files
-currentPath = pathlib.Path().absolute()
-modelName = os.path.join(currentPath, "gait10dof18musc_edit.osim")
-newModel = os.path.join(currentPath, "cycling_model.osim")
-
-# DEFINE FUNCTIONS
-# Function that finds bodies in the model
-def find_body(model,name_body):    
-    for body in model.getBodyList():    
-        if body.getName() == name_body:
-            this_body = body
-    return this_body
-
-# Function that finds joints
-def find_joint(model,name_joint):
-    for joint in model.getJointList():
-        if joint.getName() == name_joint:
-            this_joint = joint
-    return this_joint
-    
-## Parameters
-# === Pedal holder
-cleat_mass = 0.25
-cleat_inertia = osim.Inertia(1,1,1,0,0,0)
-cleat_locParent    = osim.Vec3(0.125,-0.015,0)      # (0.125,-0.018,0)
-cleat_orParent = osim.Vec3(0,0,0)
-cleat_locChild     = osim.Vec3(0,0,0)
-cleat_orChild  = osim.Vec3(0,0,0)
-
-# === Pedal
-pedal_mass = 0.5
-pedal_inertia = osim.Inertia(1,1,1,0,0,0)     
-pedal_orParent = osim.Vec3(0,0,0)
-pedal_locChild     = osim.Vec3(0,0,0)
-pedal_orChild  = osim.Vec3(0,0,0)
-
-pedal_locParent_r    = osim.Vec3(0.02,  0.133,   0.091)
-pedal_locParent_l    = osim.Vec3(0.02, -0.146, -0.091)
-
-# gear
-crank_mass = 1                              
-crank_inertia = osim.Inertia(1,1,1,0,0,0) 
-crank_locParent    = osim.Vec3(0,0,0)   
-crank_orParent = osim.Vec3(0,0,0)
-crank_locChild     = osim.Vec3(0,0,0)
-crank_orChild  = osim.Vec3(0,0,0) 
-
-# body
+# ********** Body ********** 
 pelvis_base_tx = 0.085
 pelvis_base_ty = 0
+lumbar_base = - numpy.deg2rad(50)
 
-# bike geometry
+# ********** Bike geometry ********** 
 A = 800/1000
 F = numpy.deg2rad(73.76)
 pelvis_tx_crank = -A*numpy.cos(F)
 pelvis_ty_crank = A*numpy.sin(F)
 
-# body pose
-lumbar_base = - numpy.deg2rad(50)
+# ********** Zero vector ********** 
+vector_zero = osim.Vec3(0,0,0)
 
-# === Contact forces  
-contact_loc = osim.Vec3(0,0,0)
-contact_or = osim.Vec3(0,0,0)
+# ********** Pedal ********** 
+pedal_mass = 0.5
+pedal_inertia = osim.Inertia(1,1,1,0,0,0)
+pedal_locParent_r    = osim.Vec3(0.02,  0.133,   0.091)
+pedal_locParent_l    = osim.Vec3(0.02, -0.146, -0.091)
 
+# ********** Pedal holder ********** 
+cleat_mass = 0.25
+cleat_inertia = osim.Inertia(1,1,1,0,0,0)
+cleat_locParent    = osim.Vec3(0.125,-0.015,0)      # (0.125,-0.018,0)
+
+# ********** Gear ********** 
+crank_mass = 1                              
+crank_inertia = osim.Inertia(1,1,1,0,0,0) 
+
+# ********** Contact forces  ********** 
 stiffness = 1e8
 dissipation = 25
 friction = 0.9
 viscosity = 0.6
 transition_vel = 0.1
 
-## OPEN MODEL
-# Load model
+# ********** Paths and files ********** 
+currentPath = pathlib.Path().absolute()
+modelName = os.path.join(currentPath, "gait10dof18musc_edit.osim")
+newModel = os.path.join(currentPath, "cycling_model.osim")
+
+# DEFINE FUNCTIONS
+def find_body(model,name_body):     # finds bodies
+    for body in model.getBodyList():    
+        if body.getName() == name_body:
+            this_body = body
+    return this_body
+
+def find_joint(model,name_joint): # find joints
+    for joint in model.getJointList():
+        if joint.getName() == name_joint:
+            this_joint = joint
+    return this_joint
+
+## INSTANTIATE AN OPENSIM MODEL
 myModel = osim.Model(modelName)
 myModel.setName("cycling_model")
-myState = myModel.initSystem()
 
-# Find bodies
+## GET REFERENCES TO OBJECTS
 ground = myModel.getGround()
 calcn_r = find_body(myModel,"calcn_r")
 calcn_l = find_body(myModel,"calcn_l")
 
-## MODELING GEOMETRIES AND BODIES
-# === Crank
-crank = osim.Body("crank",crank_mass,crank_orParent,crank_inertia)
+## CONSTRUCT BODIES AND JOINTS HERE
+# ********** Crank ********** 
+crank = osim.Body("crank",crank_mass,vector_zero,crank_inertia)
 
 crank_geometry = osim.Mesh("gear.stl")
 crank_geometry.setColor(osim.Black)
@@ -102,13 +80,13 @@ crank.attachGeometry(crank_geometry.clone())
 
 myModel.addBody(crank)
 
-crankToGround = osim.PinJoint("crankToGround",crank,crank_locParent,crank_orParent,ground,crank_locChild,crank_orChild)
+crankToGround = osim.PinJoint("crankToGround",crank,vector_zero,vector_zero,ground,vector_zero,vector_zero)
 
 myModel.addJoint(crankToGround)
 
-# === Cleats
-cleat_r = osim.Body("cleat_r",cleat_mass,cleat_orParent,cleat_inertia)
-cleat_l = osim.Body("cleat_l",cleat_mass,cleat_orParent,cleat_inertia)
+# ********** Cleats ********** 
+cleat_r = osim.Body("cleat_r",cleat_mass,vector_zero,cleat_inertia)
+cleat_l = osim.Body("cleat_l",cleat_mass,vector_zero,cleat_inertia)
 
 cleat_geometry = osim.Mesh("cleat_simple.stl")
 cleat_geometry.setColor(osim.Red)
@@ -118,16 +96,16 @@ cleat_l.attachGeometry(cleat_geometry.clone())
 myModel.addBody(cleat_r)
 myModel.addBody(cleat_l)
 
-cleat_r = osim.WeldJoint("cleat_rToCalc",calcn_r,cleat_locParent,cleat_orParent,cleat_r,cleat_locChild,cleat_orChild)
-cleat_l = osim.WeldJoint("cleat_lToCalc",calcn_l,cleat_locParent,cleat_orParent,cleat_l,cleat_locChild,cleat_orChild)
+cleat_r = osim.WeldJoint("cleat_rToCalc",calcn_r,cleat_locParent,vector_zero,cleat_r,vector_zero,vector_zero)
+cleat_l = osim.WeldJoint("cleat_lToCalc",calcn_l,cleat_locParent,vector_zero,cleat_l,vector_zero,vector_zero)
 myModel.addJoint(cleat_r)
 myModel.addJoint(cleat_l)
 
-# ==== Pedals
+# ********** Pedals ********** 
 cleat_r_body = find_body(myModel,"cleat_r")
 cleat_l_body = find_body(myModel,"cleat_l")
-pedal_r = osim.Body("pedal_r",pedal_mass,pedal_orParent,pedal_inertia)
-pedal_l = osim.Body("pedal_l",pedal_mass,pedal_orParent,pedal_inertia)
+pedal_r = osim.Body("pedal_r",pedal_mass,vector_zero,pedal_inertia)
+pedal_l = osim.Body("pedal_l",pedal_mass,vector_zero,pedal_inertia)
 
 pedal_geometry = osim.Mesh("pedal_simple.stl")
 pedal_geometry.setColor(osim.Orange)
@@ -137,17 +115,16 @@ pedal_l.attachGeometry(pedal_geometry.clone())
 myModel.addBody(pedal_r)
 myModel.addBody(pedal_l)
 
-pedal_rToCrank = osim.PinJoint("pedal_rToCrank",crank,pedal_locParent_r,pedal_orParent,pedal_r,pedal_locChild,pedal_orChild)
-pedal_lToCalc = osim.PinJoint("pedal_lToCalc",crank,pedal_locParent_l,pedal_orParent,pedal_l,pedal_locChild,pedal_orChild)
+pedal_rToCrank = osim.PinJoint("pedal_rToCrank",crank,pedal_locParent_r,vector_zero,pedal_r,vector_zero,vector_zero)
+pedal_lToCalc = osim.PinJoint("pedal_lToCalc",crank,pedal_locParent_l,vector_zero,pedal_l,vector_zero,vector_zero)
 
 myModel.addJoint(pedal_rToCrank)
 myModel.addJoint(pedal_lToCalc)
 
-
 ## MODELING CONTACT FORCES
 # Right
-pedal_geo_r = osim.ContactMesh("pedal_simple.stl",contact_loc,contact_or,pedal_r,"pedal_r_contact")
-cleat_geo_r = osim.ContactMesh("cleat_simple.stl",contact_loc,contact_or,cleat_r_body,"cleat_r_contact")
+pedal_geo_r = osim.ContactMesh("pedal_simple.stl",vector_zero,vector_zero,pedal_r,"pedal_r_contact")
+cleat_geo_r = osim.ContactMesh("cleat_simple.stl",vector_zero,vector_zero,cleat_r_body,"cleat_r_contact")
 myModel.addContactGeometry(pedal_geo_r)
 myModel.addContactGeometry(cleat_geo_r)
 
@@ -167,8 +144,8 @@ contact_r.addGeometry("cleat_r_contact")
 myModel.addForce(contact_r)
 
 # Left
-pedal_geo_l = osim.ContactMesh("pedal_simple.stl",contact_loc,contact_or,pedal_l,"pedal_l_contact")
-cleat_geo_l = osim.ContactMesh("cleat_simple.stl",contact_loc,contact_or,cleat_l_body,"cleat_l_contact")
+pedal_geo_l = osim.ContactMesh("pedal_simple.stl",vector_zero,vector_zero,pedal_l,"pedal_l_contact")
+cleat_geo_l = osim.ContactMesh("cleat_simple.stl",vector_zero,vector_zero,cleat_l_body,"cleat_l_contact")
 myModel.addContactGeometry(pedal_geo_l)
 myModel.addContactGeometry(cleat_geo_l)
 
@@ -227,7 +204,10 @@ pelvis_tz.setDefaultLocked (True)
 pelvis_tilt.setDefaultLocked (True)
 lumbar.setDefaultLocked (True)
 
-## SAVE
+## INITIALIZE THE SYSTEM (CHECKS MODEL CONSISTENCY)
+myState = myModel.initSystem()
+
+## SAVE THE MODEL TO A FILE
 myModel.finalizeConnections()
 myModel.printToXML(newModel)
 
