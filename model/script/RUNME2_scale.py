d29088beefecee5c5352ea26c3010be6bb6b37cf
Ana de Sousa <anacsousa1@gmail.com>
2020-05-29 17:24:12 -0300

    Created new file for scale-markers

---
+++ model/script/RUNME2_scale.py
@@ -0,0 +1,19 @@
+"""
+Created on Fri May 29 17:19:38 2020
+
+@author: Ana de Sousa (anacsousa@lara.unb.br)
+"""
+
+# IMPORT OPENSIM LIBRARIES
+import opensim as osim
+
+# IMPORT OTHER LIBRARIES
+import pathlib  # get current path
+import os.path  # create path
+import numpy    # math
+
+## DEFINE KEY MODEL VARIABLES 
+# TODO
+x = 1
+#
+
